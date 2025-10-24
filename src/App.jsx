083f6b054f5c58512045c7c6ec0cc5bb45149f47
olfalafi <olfalafi1998@gmail.com>
2025-10-24 23:07:58 +0100

    fix inputs

--- src/App.jsx
+++ src/App.jsx
@@ -11,46 +11,51 @@ function App() {
       <img src={logo} alt="Splitter Logo" />
       <div className="calculator-container">
         <div className="input-section">
+          {/* bill */}
           <div className="bill-input-container">
             <label htmlFor="name" className='input-label'>Bill</label>
-          <div className="bill-input-icon">
+            <div className="bill-input-icon">
               <img src={dollar} alt="Dollar Logo" className='bill-logo' />
-            <input
-            className='amount-input'
-              type="text"
-              id="bill"
-              name="bill"
-              required />
+              <input
+                className='amount-input'
+                type="text"
+                id="bill"
+                name="bill"
+                required />
+            </div>
           </div>
-          </div>
-          {/* select tip */}
-          <div className="bill-tip">
-            <label htmlFor="name">Select Tip % </label>
+          {/* tip */}
+          <div className="bill-tip-container">
+            <label htmlFor="name" className='input-label'>Select Tip % </label>
             <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
               <div className="btn-group mr-2" role="group" aria-label="First group">
                 <button type="button" className="btn btn-secondary">5%</button>
                 <button type="button" className="btn btn-secondary">10%</button>
               </div>
+              {/*  */}
               <div className="btn-group mr-2" role="group" aria-label="Second group">
                 <button type="button" className="btn btn-secondary">15%</button>
                 <button type="button" className="btn btn-secondary">25%</button>
               </div>
+              {/*  */}
               <div className="btn-group" role="group" aria-label="Third group">
                 <button type="button" className="btn btn-secondary">50%</button>
-                <button type="button" className="btn btn-secondary">Custom</button>
+                <button type="button" className="btn btn-secondary custom">Custom</button>
               </div>
             </div>
           </div>
-          {/* number of people */}
-          <div className="people-input">
-            <label htmlFor="name">Number Of People</label>
-            <img src={person} alt="Person Logo" className='bill-logo'/>
-
-            <input
-              type="text"
-              id="bill"
-              name="bill"
-              required />
+          {/* people */}
+          <div className="people-input-container">
+            <label htmlFor="name" className='input-label'>Number Of People</label>
+            <div className="people-input-icon">
+              <img src={person} alt="Person Logo" className='bill-logo' />
+              <input
+                className='number-people-input'
+                type="text"
+                id="bill"
+                name="bill"
+                required />
+            </div>
           </div>
         </div>
 
